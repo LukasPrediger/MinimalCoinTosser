6a1d3475eacbf01de6296b0c21c10b7fd0b813f2
Lukas Prediger <lukas.prediger@rewe-digital.com>
2023-06-08 10:54:56 +0200

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,12 +69,12 @@ kapt {
 dependencies {
     val composeVersion = "1.5.0-alpha03"
 
-    implementation("androidx.core:core-ktx:1.10.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-    implementation("androidx.compose.material3:material3:1.1.0-rc01")
+    implementation("androidx.compose.material3:material3:1.1.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
